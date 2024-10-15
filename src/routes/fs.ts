aba6072e3d1684df5e2c5c9e4e179df1f85b31fb
flamrdevs <flamrdevs@gmail.com>
2024-10-16 05:02:09 +0700

    refactor: fs route

--- src/routes/fs.ts
+++ src/routes/fs.ts
@@ -1,5 +1,7 @@
 import fs from "node:fs/promises";
 
 export async function GET() {
-  return (await fs.readdir("./", { withFileTypes: true, recursive: true })).filter((d) => d.isFile() && !d.name.startsWith("node_modules"));
+  return (await fs.readdir("./", { withFileTypes: true, recursive: true }))
+    .filter((d) => !d.name.startsWith("node_modules"))
+    .filter((d) => d.isFile());
 }
