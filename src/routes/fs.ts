1193112da864054fb838e475f4c6abf46fc704dc
flamrdevs <flamrdevs@gmail.com>
2024-10-16 05:29:46 +0700

    refactor: fonts

--- src/routes/fs.ts
+++ src/routes/fs.ts
@@ -1,17 +1,5 @@
 import fs from "node:fs/promises";
 
 export async function GET() {
-  const dirents = await fs.readdir("./", { withFileTypes: true, recursive: true });
-  const result: string[] = [];
-
-  for (const dirent of dirents) {
-    const name = dirent.name;
-    if (name.startsWith("node_modules/public")) {
-      result.push(name);
-    } else if (!name.startsWith("node_modules")) {
-      result.push(name);
-    }
-  }
-
-  return result;
+  return await fs.readdir("./", { recursive: true });
 }
