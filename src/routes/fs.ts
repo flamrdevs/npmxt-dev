b11e839ee582712965afa0443d54548a84502c5c
flamrdevs <flamrdevs@gmail.com>
2024-10-15 23:57:59 +0700

    test: fs route

--- src/routes/fs.ts
+++ src/routes/fs.ts
@@ -1,8 +1,23 @@
 import fs from "node:fs/promises";
+import path from "node:path";
 
 export async function GET() {
   return {
-    "./": await fs.readdir("./"),
-    "cwd()": await fs.readdir(process.cwd()),
+    "./": {
+      "./": await fs.readdir("./"),
+      "cwd()": await fs.readdir(process.cwd()),
+    },
+    "./../": {
+      "./": await fs.readdir("./../"),
+      "cwd()": await fs.readdir(path.resolve(process.cwd(), "../")),
+    },
+    "./../../": {
+      "./": await fs.readdir("./../../"),
+      "cwd()": await fs.readdir(path.resolve(process.cwd(), "../../")),
+    },
+    "./../../../": {
+      "./": await fs.readdir("./../../../"),
+      "cwd()": await fs.readdir(path.resolve(process.cwd(), "../../../")),
+    },
   };
 }
