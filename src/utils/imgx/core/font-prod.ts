9309703524bc02b55b9d58c64c2f5a08db7bb803
flamrdevs <flamrdevs@gmail.com>
2024-10-16 02:05:11 +0700

    refactor: assets fonts

---
+++ src/utils/imgx/core/font-prod.ts
@@ -0,0 +1,14 @@
+import type { Font } from "satori";
+
+const file = async (source: string, name: string, style?: Font["style"], weight?: Font["weight"]) => ({
+  data: (await import(source)).default,
+  name,
+  style,
+  weight,
+});
+
+export const load = async () => {
+  return await Promise.all(
+    ([300, 400, 500, 600, 700] as const).map((weight) => file(`./../raw/source-code-pro-${weight}`, "source-code-pro", "normal", weight))
+  );
+};
